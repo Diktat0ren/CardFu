3090815162436adb94df1e7e7b899fb7b2590d61
Diktat0ren <duckandersduck@gmail.com>
2016-03-06 05:10:46 +0100

    Added option for more songs
    The konami code should now randomly pick one of the three songs. Only spacecats are added for now, i'll add the others later.

--- public/javascripts/konami.js
+++ public/javascripts/konami.js
@@ -1,6 +1,22 @@
-spacecats = new Audio("../sound/spacecats.mp3");
-spacecats.volume = 0.5;
-spacecats.addEventListener("ended", function() {
+
+var song "";
+switch(Math.floor((Math.random() * 3) + 1)) {
+	case 1:
+		song = "spacecats.mp3";
+		break;
+	case 2:
+		song = "catsong2.mp3";
+		break;
+	case 3:
+		song = "catsong3.mp3";
+		break;
+}
+	
+songPlaying = new Audio("../sound/+song);
+
+
+songPlaying.volume = 0.5;
+songPlaying.addEventListener("ended", function() {
 	this.currentTime = 0;
 	this.play();
 }, false);
@@ -16,10 +32,10 @@ $(document).keydown(function(e){
 		if(konami_index === konami_keys.length){
 			konami_index = 0;
 			if (playing) {
-				spacecats.pause();
+				songPlaying.pause();
 				playing = false;
 			} else {
-				spacecats.play();
+				songPlaying.play();
 				playing = true;
 			}
 		}
